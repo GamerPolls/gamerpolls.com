4b4e1595641eadd626afb6ad2dcca3b5aa319ba9
Ryan Chatham <uancletus@gmail.com>
2013-11-12 20:22:05 -0800

    Fix site name

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -3,7 +3,7 @@ var Controller = locomotive.Controller;
 var PagesController = new Controller();
 
 PagesController.main = function() {
-	this.title = 'TwitchPolls';
+	this.title = 'TwitchPoll';
 	this.render();
 };
 
