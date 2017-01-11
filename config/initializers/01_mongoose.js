fbe2b5060102589c1976e00fba7ad238aec38f11
Muddr <truckmuddr@gmail.com>
2017-01-11 14:18:13 -0500

    Updates mongoose Promise changes

--- config/initializers/01_mongoose.js
+++ config/initializers/01_mongoose.js
@@ -4,5 +4,6 @@ var nconf = require('nconf');
 module.exports = function () {
 	this.mongoose = require('mongoose');
 	this.mongoose.connect(nconf.get('MONGO_DB'));
+	this.mongoose.Promise = global.Promise;
 	autoIncrement.initialize(this.mongoose.connection);
 };
