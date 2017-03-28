f7d5d1c5b7641b2ebf143429ed6a9d363b580379
Muddr <truckmuddr@gmail.com>
2017-03-28 13:38:14 -0400

    Fixed changes to Twitch API and updates to v0.4.1

--- config/initializers/04_twitchapi.js
+++ config/initializers/04_twitchapi.js
@@ -2,6 +2,7 @@ var twitch = require('node-twitch-api');
 var nconf = require('nconf');
 
 module.exports = function () {
-	twitch.clientID = nconf.get('authkeys:twitchtv:clientID');
+	twitch.defaults.clientID = nconf.get('authkeys:twitchtv:clientID');
+	twitch.defaults.apiVersion = nconf.get('authkeys:twitchtv:apiVersion');
 	this.twitch = twitch;
 };
